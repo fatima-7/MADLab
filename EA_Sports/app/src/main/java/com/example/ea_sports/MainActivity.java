0c91d340f1f9e58aa9858b1b321737d8b176132b
fatima-7 <fatmahzeb6@gmail.com>
2020-10-29 14:06:34 +0500

    Version 1.0 of our semester project

---
+++ EA_Sports/app/src/main/java/com/example/ea_sports/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ea_sports;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
